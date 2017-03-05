124167168dec5345d5614b3979bd38e838354a08
evgenii <mir-evgenii@mail.ru>
2017-03-05 22:40:17 +0300

    edit_user

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -50,5 +50,26 @@ if ($this->request->is('post')) {
  return $this->redirect($this->Auth->logout());
 	}
 
+	public function edit ($id = null){
+		if (!$this->User->exists($id)) {
+			throw new NotFoundException(__('Такого пользователя нет!'));
+		}
+		$user = $this->User->findById($id);
+		//$this->set('categories', $this->Post->Category->find('list'));
+		$this->set('user', $user);
+
+		if ($this->request->is(['user', 'put'])) {
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash('Настройки пользователя обновлены');
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Session->setFlash('Ошибка обновления настроек пользователя');
+		}
+
+		if (!$this->request->data) {
+			$this->request->data = $user;
+		}
+	}
+
 }
 ?>
--- View/Users/index.ctp
+++ View/Users/index.ctp
@@ -6,16 +6,20 @@
 <table>
 <tr>
 <th>Id</th>
-<th>Логин</th>
-<th>Пароль</th>
+<th>Имя</th>
+<th>Адрес</th>
+<th>Телефон</th>
 <th>Роль</th>
+<th>Действия</th>
 </tr>
 <?php foreach ($users as $user) : ?>
 <tr>
 <td><?= $user['User']['id'] ?></td>
 <td><?= h($user['User']['username']) ?></td>
-<td><?= $user['User']['password'] ?></td>
+<td><?= $user['User']['address'] ?></td>
+<td><?= $user['User']['phone'] ?></td>
 <td><?= $user['User']['role'] ?></td>
+<td><?= $this->Html->link('Изменить', ['action' => 'edit', $user['User']['id']]) ?></td>
 </tr>
 <?php endforeach;?>
 </table>
